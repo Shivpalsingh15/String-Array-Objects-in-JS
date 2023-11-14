ea1343d993dcb5177fa60119c09836f0dba4fe7b
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2023-11-14 22:26:32 +0530

    String Method

---
+++ practise.js
@@ -0,0 +1,28 @@
+let arr1= [1,  2, 3, 4]
+let arr2 = ['kuldeep', 'shiv', 'nilesh']
+console.log('arr1:', arr1.length)
+console.log('arr1:', arr1[2])
+console.log('arr1:', arr1.includes(4))
+console.log('arr1:', arr1.indexOf(2))
+console.log('arr6:', arr1.push(6))
+console.log('arr1:', arr1)
+console.log('arr6:', arr1.pop())
+console.log('arr7:', arr1.push(...arr2))
+console.log('arr1:', arr1)
+console.log('arr1:', arr1.shift())
+console.log('arr1:', arr1)
+console.log('arr1:', arr1.unshift("Taku"))
+// console.log('arr1:', arr1.filter((el, index)=> index< 3))
+console.log('arr1:', arr1.find((el)=> el>1))
+console.log('arr1:', arr1.flat())
+
+
+
+
+
+
+
+
+
+
+
---
+++ stringMethod.js
@@ -0,0 +1,56 @@
+// 1) .at(index) return the index value of string when we pass the index as argument
+// let str  = 'taku is good boy'
+// console.log(str.at(5))
+ 
+
+/*
+
+2)   .charAt(index) return the index value of string when we pass the index as argument
+// let str  = 'taku is good boy'
+// console.log(str.at(5))
+
+
+*/
+// 3) .includes 
+//method which determines wheither the element exits in string and return boolean value.
+// let str1= "My house is in khargone"
+// console.log(str1.includes("house")? "yes ": "n")
+
+
+
+
+/*
+
+The split() method of String values takes a pattern and divides this string into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]);
+// Expected output: "fox"
+
+const chars = str.split('');
+console.log(chars[8]);
+// Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
+
+*/
+/*
+.toLowerCase()
+.toUpperCase()
+ex : const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.split(' ')[3].toUpperCase());  <-- this is somthing tricky ..!
+console.log(sentence);
+// Expected output: "the quick brown fox jumps over the lazy dog."
+
+
+trim() remove sapce from both side
+trimEnd() remove spaces at the end
+trimStart() remove spaces at the starting
+
+
+*/
